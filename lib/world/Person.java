fb6be7deb423d75b05c844e6c98c9e5d3ac5da50
Ramsay <mudpiegon@outlook.com>
2023-07-22 23:37:30 +0800

    添加了一个包 lib.world 用于存储一些用于测试的代表实际事物的类

---
+++ lib/world/Person.java
@@ -0,0 +1,48 @@
+package lib.world;
+
+public class Person {
+    // 属性
+    public static final boolean MALE = true;
+    public static final boolean FEMALE = false;
+    private String name;
+    private int age;
+    private boolean gender;
+
+    // 构造方法
+    public Person(String name, int age, boolean gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    // 方法
+    public void introduce() {
+        System.out.println("My name is " + name + ".");
+        System.out.println("I am " + age + " years old.");
+        if (this.gender) System.out.println("My gender is male.");
+        else             System.out.println("My gender is female.");
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        if (this.gender) return "male";
+        else             return "female";
+    }
+}
+
