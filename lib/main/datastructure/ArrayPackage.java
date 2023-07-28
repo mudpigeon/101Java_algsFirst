129e7198ec3dc3858eefe1ea70895a5c3e0c5dd4
Ramsay <mudpiegon@outlook.com>
2023-07-28 12:55:16 +0800

    实现了基于数组结构的 package 数据结构

--- lib/main/datastructure/ArrayPackage.java
+++ lib/main/datastructure/ArrayPackage.java
@@ -1,10 +1,116 @@
 package lib.main.datastructure;
 
+import java.util.Iterator;
 
+import lib.api.AbstractPackage;
+import lib.std.StdOut;
+import lib.std.StdRandom;
 
 /**
  * 一个基于数组结构实现的泛型不定容包
  */
-public class ArrayPackage<Item> implements  {
-    
+public class ArrayPackage<Item> implements AbstractPackage<Item> {
+
+    private int pointer;
+    private Item[] contents;
+    private int len;
+
+    public ArrayPackage(int initialCapacity) {
+        this.contents = (Item[]) new Object[initialCapacity];
+        this.pointer = 0;
+        this.len = initialCapacity;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ReverseArrayFormIterator();
+    }
+
+    private class ReverseArrayFormIterator implements Iterator<Item> {
+
+        private int i = pointer;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        @Override
+        public Item next() {
+            return contents[--i];    
+        }
+
+    }
+
+    @Override
+    public void add(Item item) {
+        if (4 * pointer > 3 * len) {
+            resize(len * 2);
+        }
+        contents[pointer] = item;
+        pointer++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.pointer == 0;
+    }
+
+    @Override
+    public int size() {
+        return this.pointer;
+    }
+
+    /**
+     * 返回 package 的最大容量
+     * @return
+     */
+    public int supremeSize() {
+        return this.len;
+    }
+
+    /**
+     * 当 package 容量达到某个程度时，修改 package 的大小
+     * @param cap
+     */
+    private void resize(int cap) {
+        var temp = (Item[]) new Object[cap];
+        int index = 0;
+        for (index = 0; index < pointer; index++) {
+            temp[index] = contents[index]; 
+        }
+
+        contents = temp;
+        len = cap;
+    }
+
+    public static void main(String[] args) {
+        var stats = new ArrayPackage<Integer>(20);
+        StdOut.println("初始包容量: " + stats.supremeSize());
+        StdOut.println("是否为空: " + stats.isEmpty());
+        StdOut.println("当前包大小为: " + stats.size());
+
+
+        for (int i = 0; i < 15; i++) {
+            stats.add(StdRandom.uniformInt(1000));
+        }
+        
+        StdOut.println("第一次添加后包容量: " + stats.supremeSize());
+        StdOut.println("当前包大小为: " + stats.size());
+
+        // 容量自动增长测试
+        for (int i = 0; i < 15; i++) {
+            stats.add(StdRandom.uniformInt(1000));
+        }
+        
+        StdOut.println("第二次添加后包容量: " + stats.supremeSize());
+        StdOut.println("当前包大小为: " + stats.size());
+
+        int sum = 0;
+        for (Integer i : stats) {
+            sum += i;
+        }
+        StdOut.println("总和为: " + sum);
+
+    }
 }
