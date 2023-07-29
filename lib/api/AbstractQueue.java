abd182c21615fef0439acbe5dea80ab25e3d100f
Ramsay <mudpiegon@outlook.com>
2023-07-29 11:23:24 +0800

    修改方法名

--- lib/api/AbstractQueue.java
+++ lib/api/AbstractQueue.java
@@ -6,13 +6,13 @@ public interface AbstractQueue<Item> extends Iterable<Item> {
      * 向 Queue 中添加一个元素
      * @param item
      */
-    void enQueue(Item item);
+    void enqueue(Item item);
 
     /**
      * 根据 Queue 数据结构的特性，弹出最早加入的元素
      * @return Item item
      */
-    Item deQueue();
+    Item dequeue();
 
     /**
      * 判断队列是否为空
